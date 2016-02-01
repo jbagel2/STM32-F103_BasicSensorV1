78a818a5512723fb6266aa5f94d04749f798904d
Jacob Pagel <jacob.k.pagel@gmail.com>
2016-01-31 20:47:05 -0700

    Initial for working basic Sensor Code

---
+++ BasicSensor/src/ESP8266/ATFirm/atfirm.h
@@ -0,0 +1,15 @@
+/*
+ * atfirm.h
+ *
+ *  Created on: Dec 26, 2015
+ *      Author: Jacob
+ */
+
+#ifndef __ATFIRM_H_
+#define __ATFIRM_H_
+
+const char *ATCommandsArray[20];
+const char *ESP_Responses[10];
+
+
+#endif /* __ATFIRM_H_ */
---
+++ BasicSensor/src/Sys_Config/Sys_Config.h
@@ -0,0 +1,23 @@
+/*
+ * Sys_Config.h
+ *
+ *  Created on: Dec 20, 2015
+ *      Author: Jacob
+ */
+
+#ifndef SYS_CONFIG_H_
+#define SYS_CONFIG_H_
+
+/* Global Statics/Macros */
+char *FIRMWARE_VERSION;
+
+
+/* Global Variables */
+char *NODE_ID;
+#define RxBuffSize 300
+
+
+/* Public Sys_Config Functions */
+
+
+#endif /* SYS_CONFIG_H_ */
